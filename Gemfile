4b5ee36a26435c74f2fc14def1e6a5b3bb8ae9e7
Eduardo Gonzalez <egonalez@oncallair.com>
2018-10-14 17:28:55 -0400

    added signature page to views, assets

--- Gemfile
+++ Gemfile
@@ -67,4 +67,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bootstrap', '~> 4.1.3'
 gem  'jquery-rails'
+gem 'jquery-ui-rails'
 gem "font-awesome-rails"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  root 'forms#index'
   devise_for :users
   resources :forms
+  devise_scope :user do
+    authenticated :user do
+      root 'forms#new', as: :authenticated_root
+    end
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
