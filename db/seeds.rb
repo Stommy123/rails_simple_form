c9695da83967a51f59fd1142cf60d09948d291b4
Stommy123 <Stommy49@Yahoo.com>
2018-10-14 17:37:27 -0400

    created seed file for products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.destroy_all
+
+Product.create(description: "Alpha", image: "https://i.imgur.com/jIWfhU0.png")
+Product.create(description: "Beta", image: "https://i.imgur.com/gLjN8jg.png")
+Product.create(description: "Gamma", image: "https://i.imgur.com/gLjN8jg.png")
