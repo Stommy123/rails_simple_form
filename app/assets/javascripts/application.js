ba42b030d0395e46844cc7ff11e2559abe0ffa2b
Stommy123 <Stommy49@Yahoo.com>
2018-10-18 10:19:05 -0400

    jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,14 +18,15 @@
 //= require signature_pad
 //= require_tree .
 
+console.log("hello")
 
 $(document).on('turbolinks:load', function() {
   var canvas = document.querySelector("canvas");
   if (canvas){
     canvas.height = canvas.offsetHeight;
     canvas.width = canvas.offsetWidth;
-    window.onresize = resizeCanvas(canvas);
-    resizeCanvas(canvas);
+    // window.onresize = resizeCanvas(canvas);
+    // resizeCanvas(canvas);
     signature_pad = new SignaturePad(canvas);
     $('.signature_pad_clear').click(function() { signature_pad.clear() });
     $('.signature_pad_save').click(function(event) {
